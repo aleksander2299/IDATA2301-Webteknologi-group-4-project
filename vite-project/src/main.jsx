60e1bb345b3ef26135d0efd9e3a95549f83e765d
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-04-23 15:43:42 +0200

    imported homepage to react

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import LoginPage from './components/LoginPage.jsx'
-//import Register from './components/Register.jsx'
-import Settings from './components/Settings.jsx'
+import HomePage from './components/Homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage/>
+    <HomePage/>
   </StrictMode>,
 )
