c40ad7ff0ecb4f1a6eeaba4e8d1d34632c225907
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-05-05 17:54:36 +0200

    added login and sign up buttons

--- vite-project/src/components/layout/Header.jsx
+++ vite-project/src/components/layout/Header.jsx
@@ -1,20 +1,42 @@
 import { Link } from 'react-router-dom';
-
 import '../../styles/main.css';
 
-
 function Header() {
-    return (
+  return (
+    <header style={{ 
+      display: 'flex', 
+      justifyContent: 'space-between', 
+      alignItems: 'center', 
+      padding: '0 1rem', 
+      height: '5rem',
+      width: '100%',
+      boxSizing: 'border-box',
+      overflowX: 'hidden'
+    }}>
+      {/* Stay Finder on the left */}
+      <Link to="/">
+        <button className="home-buttons">
+          <b>Stay Finder</b>
+        </button>
+      </Link>
 
-        <header>
-            <Link to={'/'} >
-                <button className="home-buttons">
-                    <b>Stay Finder</b>
-                </button>
-            </Link>
-        </header>
+      {/* Container for buttons on the right */}
+      <div className="right-buttons" style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Link to="/login">
+          <button className="home-buttons right-button">
+            <b>Log In</b>
+          </button>
+        </Link>
 
-    );
+        {/* Add a third button */}
+        <Link to="/register">
+          <button className="home-buttons right-button">
+            <b>Sign Up</b>
+          </button>
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
