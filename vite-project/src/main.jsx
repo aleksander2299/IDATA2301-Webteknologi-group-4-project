01e787943a7ea3982e02ddbf01a4d88373a58825
MoldyDaniel <72633612+MoldyDaniel@users.noreply.github.com>
2025-05-05 16:25:04 +0200

    Added module.css viabilty to RoomDetailsPage

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -6,8 +6,8 @@ import HomePage from './pages/HomePage/HomePage.jsx';
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
 import SearchPage from './pages/SearchPage/SearchPage.jsx';
-import RoomDetailsPage from './pages/RegisterPage/RoomDetailsPage.jsx';
-//"./pages/RoomDetailsPage/RoomDetailsPage.jsx"
+import RoomDetailsPage from './pages/RoomDetailsPage/RoomDetailsPage.jsx';
+
 const router = createBrowserRouter ([
   { path: '/', element: <HomePage /> },
   { path: '/login', element: <LoginPage /> },
--- vite-project/src/pages/RoomDetailsPage/RoomDetailsPage.jsx
+++ vite-project/src/pages/RoomDetailsPage/RoomDetailsPage.jsx
@@ -1,6 +1,7 @@
 import '../../styles/main.css';
 import './RoomDetailsPage.css';
 
+
 import Footer from '../../components/layout/Footer.jsx';
 
 import roomImg from '../../Images/room image placeholder.jpg';
@@ -9,54 +10,51 @@ function RoomDetailsPage () {
     return (
         <div>
             <header>
-                <div class="home"><b>Stay Finder</b></div>
+                <div className="home"><b>Stay Finder</b></div>
             </header>
-            <div class="content-container">
-                <h1 class="roomnametext">Thon Hotel Ålesund, City Centre. Ålesund, Norway.</h1>
+            <div className="content-container">
+                <h1 className="roomnametext">Thon Hotel Ålesund, City Centre. Ålesund, Norway.</h1>
             </div>
-            <section class="content-container">
-                <div class="image">
+            <section className="content-container">
+                <div className="image">
                     <img src={roomImg} alt="RoomDetailsPlaceholderImg"/>
                 </div>
-                <section class="bookingbox">
-                    <div class="bookingboxtext">How long will you stay?</div>
-                    <button class="button1">Start Date</button>
-                    <button class="button2">End Date</button>
-                    <button class="button3">BOOK NOW!</button>
+                <section className="bookingbox">
+                    <div className="bookingboxtext">How long will you stay?</div>
+                    <button className="button1">Start Date</button>
+                    <button className="button2">End Date</button>
+                    <button className="button3">BOOK NOW!</button>
                 </section>
             </section>
-            <section class="content-container">
-                <div class="detailsbox">
-                    <div class="description">
-                        <h2 class="bigwhitetext">Description</h2>
-                        <p class="smallwhitetext">
+            <section className="content-container">
+                <div className="detailsbox">
+                    <div className="description">
+                        <h2 className="bigwhitetext">Description</h2>
+                        <p className="smallwhitetext">
                             Lovely hotel suite in the heart of Ålesund! You and your partner get one of the best views the city has to offer overlooking the bay,
                             a king-sized bed with maximum comfortability and wonderful room service!
-                    
+
                         </p>
                     </div>
-                    <div class="site-specification">
-                        <h2 class="bigwhitetext">Room Specifications</h2>
-                        <p class="smallwhitetext">
-                            - Room type: Suite<br />
-                            - Bed type: Double<br />
-                            - Room capacity: 2<br />
-                            - Check-in: 3:00 PM<br />
-                            - Check-out: 11:00 AM<br />
-                            - Internet: Included<br />
-                            - Parking: Available<br />
-                            - Gym: Available<br />
-                            - Pet Friendly: Yes<br />
+                    <div className="site-specification">
+                        <h2 className="bigwhitetext">Room Specifications</h2>
+                        <p className="smallwhitetext">
+                            Room type: Suite<br />
+                            Bed type: Double<br />
+                            Room capacity: 2<br />
+                            Check-in: 3:00 PM<br />
+                            Check-out: 11:00 AM<br />
+                            Internet: Included<br />
+                            Parking: Available<br />
+                            Gym: Available<br />
+                            Pet Friendly: Yes<br />
                         </p>
                     </div>
                 </div>
-            </section>
-            <Footer />
+            </section><Footer />
         </div>
     );
 
 }
 
 export default RoomDetailsPage;
-
-
