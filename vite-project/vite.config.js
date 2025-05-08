8184edf1d4f5eedc7494af2d54b2a5dee5e1d48b
aleksander2299 <aleksander-nesvik@hotmail.com>
2025-05-08 13:40:00 +0200

    force react to laucnho n port 5173

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,9 +4,13 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173
+  },
   css: {
     modules: {
       generateScopedName: '[name]__[local]___[hash:base64:5]', // Scoping the CSS class names
     },
+    
   },
 })
