e903b1ecd2dd64cd1d192b5f1fcdf7fafa9abfaf
nikoaa <nikoaa@stud.ntnu.no>
2025-05-15 18:06:43 +0200

    Added folder types for types used across mutliple files and remove in FavouritesPage now works

--- vite-project/src/pages/FavouritesPage/FavouritesPage.tsx
+++ vite-project/src/pages/FavouritesPage/FavouritesPage.tsx
@@ -7,15 +7,22 @@ import '../../styles/main.css';
 
 import Footer from '../../components/layout/Footer.tsx';
 import Header from '../../components/layout/Header.tsx';
-import FavouriteRoomCard from "../../components/favouriteRoomCard/FavouriteRoomCard";
+import FavouriteRoomCard from "../../components/favouriteRoomCard/FavouriteRoomCard"
+import { Room } from '../../types/Room';
 
 function FavouritesPage () {
 
-    const [rooms, setRooms] = useState([]);
+    const [favourites, setFavourites] = useState<Favourite[]>([]);
     const [loading, setLoading] = useState(true);
 
     const username = localStorage.getItem('username');
     const token = localStorage.getItem('token');
+
+    type Favourite = {
+        favouriteId: number;
+        room: Room;
+        user: any; // or you can make this more specific if needed later
+    };
 
     useEffect(() => {
         async function fetchFavourites() {
@@ -25,7 +32,7 @@ function FavouritesPage () {
                         Authorization: `Bearer ${token}`
                     }
                 });
-                setRooms(response.data);
+                setFavourites(response.data);
             } catch (err) {
                 console.log(err);
                 console.log("username here: " + username);
@@ -44,9 +51,13 @@ function FavouritesPage () {
                 <h1>Favorite rooms</h1>
                 <section id={favPageStyles.favourites_container}>
                     {loading && <p>Loading...</p>}
-                    {!loading && rooms.length === 0 && <p>No favourite rooms found.</p>}
-                    {!loading && rooms.map((room) => (
-                        <FavouriteRoomCard key={room.roomId} room={room} />
+                    {!loading && favourites.length === 0 && <p>No favourite rooms found.</p>}
+                    {favourites.map((favourite) => (
+                        <FavouriteRoomCard
+                            key={favourite.favouriteId}
+                            room={favourite.room}
+                            favouriteId={favourite.favouriteId}
+                        />
                     ))}
                 </section>
             </main>
---
+++ vite-project/src/types/Room.ts
@@ -0,0 +1,9 @@
+export type Room = {
+    roomId: number;
+    source: any;
+    roomName: string;
+    description?: string;
+    visibility: boolean;
+    roomType: string;
+    imageUrl: string;
+};
