615b279842003c8b56c98b32cdb0f4dedead2400
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-05-15 16:13:43 +0200

    set up basic provider page file and made header buttons for it

---
+++ vite-project/src/pages/ProviderPage/ProviderPage.tsx
@@ -0,0 +1,17 @@
+import '../../styles/main.css';
+
+import Footer from '../../components/layout/Footer';
+import Header from '../../components/layout/Header';
+
+function ProviderPage() {
+
+    return (
+        <>
+            <Header />
+
+            <Footer />
+        </>
+    );
+}
+
+export default ProviderPage;
