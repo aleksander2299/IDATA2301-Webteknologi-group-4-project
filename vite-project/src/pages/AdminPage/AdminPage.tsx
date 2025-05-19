6388c14ee5ed961bda582fc432772e127a44c994
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-05-19 14:59:50 +0200

    fixed basic layout of admin page

--- vite-project/src/pages/AdminPage/AdminPage.tsx
+++ vite-project/src/pages/AdminPage/AdminPage.tsx
@@ -5,45 +5,22 @@ import Header from '../../components/layout/Header.jsx';
 
 function AdminPage() {
   return (
-    <main>
+    <>
       <Header />
+      <div className={adminStyle.adminpagebody}>
+        {/* Search bar component */}
+        <section>
 
-      {/* Main content */}
-      <div className={adminStyle.adminPageBody}>
-        <div className={adminStyle.container}>
+        </section>
 
-          {/* Top Buttons */}
-          <div className={adminStyle["top-buttons"]}>
-            <button className={adminStyle["nav-button"]}>Rooms</button>
-            <button className={adminStyle["nav-button"]}>Provider</button>
-          </div>
-
-          {/* Search Bar */}
-          <div className={adminStyle["search-bar"]}>
-            <form className={adminStyle["search-field"]}>
-              <label htmlFor="Search">Search for hotel/location</label>
-              <input type="search" name="Search" id="Search" />
-            </form>
-            <button className={adminStyle["search-btn"]}>Search</button>
-          </div>
-
-          {/* Card */}
-          <div className={adminStyle["admin-card"]}>
-            <div className={adminStyle["image-placeholder"]}>Image</div>
-            <div className={adminStyle["card-info"]}>
-              <p>Lorem Ipsum, Lorem Ipsum, Lorem Ipsum</p>
-            </div>
-            <div className={adminStyle["card-filters"]}>
-              <button className={adminStyle["btn-edit"]}>Edit</button>
-              <button className={adminStyle["btn-hide/show"]}>Hide</button>
-              <button className={adminStyle["btn-delete"]}>Delete</button>
-            </div>
-          </div>
-        </div>
+        {/* Hotelcard component */}
+        <section>
+          
+        </section>
       </div>
 
       <Footer />
-    </main>
+    </>
   );
 }
 
