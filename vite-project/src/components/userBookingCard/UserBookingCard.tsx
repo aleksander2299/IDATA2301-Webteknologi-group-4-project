2c0f118ba6bd967fe2624064d021c991cb5a8a7e
nikoaa <nikoaa@stud.ntnu.no>
2025-05-20 01:19:15 +0200

    Updated userBooking page. It should only be missing the proper styling

--- vite-project/src/components/userBookingCard/UserBookingCard.tsx
+++ vite-project/src/components/userBookingCard/UserBookingCard.tsx
@@ -2,18 +2,56 @@ import userBookingCardStyle from './UserBookingCard.module.css'
 import '../../styles/main.css';
 import {Room} from '../../types/Room.ts'
 
-import roomImg from '../../Images/room image placeholder.jpg';
+import {Link} from "react-router-dom";
+import {Booking} from "../../types/Booking.ts";
+import {axiosInstance} from "../../AxiosInstance.tsx";
+import {useState} from "react";
+
+const token = localStorage.getItem('token');
 
 type UserBookingCardProps = {
-    bookingId: number;
+    booking: Booking;
     room: Room;
 };
 
-function UserBookingCard ({ bookingId, room }: UserBookingCardProps) {
+function UserBookingCard ({ booking, room }: UserBookingCardProps) {
+
+    const [disabled, setDisabled] = useState(false);
+    const [buttonText, setButtonText] = useState("Cancel");
+
+    const handleCancellation = async () => {
+        try {
+            const response = await axiosInstance.delete(`/booking/${booking.bookingId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            if (response.status === 200 || response.status === 204) {
+                setDisabled(true);
+                setButtonText("Cancelled");
+            } else {
+                // Optional: handle unexpected response
+                setButtonText("Error");
+            }
+        } catch (err) {
+            console.error("Failed to cancel booking:", err);
+            setButtonText("Error");
+        }
+
+
+
+    }
+
     return (
-        <div className={userBookingCardStyle.favourite_card}>
-            <img src={room.imageUrl} alt={room.roomName} />
-            <button className="removeFavorite">Remove</button>
+        <div className={userBookingCardStyle.booking_card}>
+            <Link to={`/room/${room.roomId}`}>
+                <img src={room.imageUrl} alt={room.roomName} />
+            </Link>
+            <p className="roomName">{room.roomName}</p>
+            <p className="startTime">Start: {booking.checkInDate}</p>
+            <p className="endTime">End: {booking.checkOutDate}</p>
+            <button className="cancelButton" onClick={handleCancellation} disabled={disabled}>{buttonText}</button>
         </div>
     );
 
