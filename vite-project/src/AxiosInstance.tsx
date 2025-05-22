8671ad05aec18ab51d26ad46be1c49eeed962e44
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-05-22 17:53:08 +0200

    added info to run locally or not

--- vite-project/src/AxiosInstance.tsx
+++ vite-project/src/AxiosInstance.tsx
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: `/api`
+    baseURL: `http://localhost:8080/api`
 });
+
+
+{/* CHANGE THESE TO WHAT YOU WANT ABOVE */}
+
+{/* LOCALHOST baseURL to run site locally: */}
+{/* baseURL: `http://localhost:8080/api` */}
+
+{/* DEPLOYMENT baseURL: */}
+{/* baseURL: `/api `*/}
